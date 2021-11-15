a911c94c6a20e51e52e903cb76a5847d66c1265f
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-15 17:05:52 +0000

    close led stuff

--- led_with_shifter.py
+++ led_with_shifter.py
@@ -6,12 +6,14 @@ dataPin, latchPin, clockPin = 21, 20, 16 # data pins for shift registers
 
 
 try: # exception handling
-  theLEDdisplay= LED8x8(dataPin, latchPin, clockPin) # create LED display object from class
+  LEDdisplay= LED8x8(dataPin, latchPin, clockPin) # create LED display object from class
 
 # More exception handling:
 except KeyboardInterrupt: 
   print('\nExiting')
 except Exception as e: # catch all other errors
-  print('\n', e)
+  print(e)               # delete once code is debugged
+  LEDdisplay.p.terminate()      # terminate the process
+  LEDdisplay.p.join(2)          # wait up to 2 sec for process termination before ending code
 
 GPIO.cleanup()
