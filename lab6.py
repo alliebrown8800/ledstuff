466f9c58e255d05597733b71d7616501ce7820f4
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-15 18:28:50 +0000

    ok

--- lab6.py
+++ lab6.py
@@ -4,7 +4,7 @@ from LED8X8 import LED8x8 # importing LED8x8 class
 dataPin, latchPin, clockPin = 21, 20, 16 # data pins for shift registers
 
 try: # exception handling
-  LEDdisplay= LED8x8(dataPin, latchPin, clockPin) # create LED display object from class
+  LEDdisplay = LED8x8(dataPin, latchPin, clockPin) # create LED display object from class
   print('ok')
   while True:
     print('hi')
