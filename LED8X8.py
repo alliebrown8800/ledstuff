744063dbbaa60fedc7a71142eee64021b9d7dca8
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-15 16:13:53 +0000

    smile

--- LED8X8.py
+++ LED8X8.py
@@ -1,14 +1,3 @@
-# Smilie face: Create a new LED8x8 class that calls the shiftByte method of the Shifter class
-# twice to send 2 bytes to the pair of SRs: create a display method within LED8x8 that
-# sequentially sends 8 pairs of bytes to a Shifter object. The first byte will define the row pattern
-# (taken from the global pattern variable), and the second byte will select the current row. The
-# method should do this sequentially to display all 8 rows. Repeat this in an infinite loop with a 0.001
-# s delay between each cycle. Test out your code with the following pattern for a smilie face:
-# pattern = [0b00111100, 0b01000010, 0b10100101, 0b10000001,
-# 0b10100101, 0b10011001, 0b01000010, 0b00111100]
-# You may find that your smilie is upside down, since the LED rows are numbered from the top to
-# Bottom – you can correct this by changing the row selection byte in the display method…
-
 from shifter import Shifter # extend by composition
 import time
 
