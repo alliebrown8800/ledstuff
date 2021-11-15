f069cba59fc8f3bf14f49c0325d1f1b0723d58dc
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-15 18:24:43 +0000

    stress

--- LED8X8.py
+++ LED8X8.py
@@ -12,17 +12,16 @@ class LED8x8():
   
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock) # initiate with pins
-    p = multiprocessing.Process(target=self.display(LED8x8.pattern), args = self.pattern)
+    p = multiprocessing.Process(target=self.display(LED8x8.pattern), args = (self.pattern))
     p.daemon = True
     p.start()
 
   def display(self, pattern): # display a given pattern (where pattern is a list of bytes)
-    while True:
-      for row in range(8): # for each of the 8 rows
-        self.shifter.shiftByte(pattern[row]) # display pattern on that row
-        self.shifter.shiftByte(1 << (row)) # select the given row 
-        self.shifter.latch() # latch the shift registers
-        time.sleep(.001) # sleep for 1 ms
+    for row in range(8): # for each of the 8 rows
+      self.shifter.shiftByte(pattern[row]) # display pattern on that row
+      self.shifter.shiftByte(1 << (row)) # select the given row 
+      self.shifter.latch() # latch the shift registers
+      time.sleep(.001) # sleep for 1 ms
   
   def randomwalk(self):
     y_change = random.choice(self.movement) # change in y direction
