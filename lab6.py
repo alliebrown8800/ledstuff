ffb9e562f3b0d04c02446390c1f30351de36ba18
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-15 18:21:52 +0000

    pls print ok

--- lab6.py
+++ lab6.py
@@ -5,6 +5,7 @@ dataPin, latchPin, clockPin = 21, 20, 16 # data pins for shift registers
 
 try: # exception handling
   LEDdisplay= LED8x8(dataPin, latchPin, clockPin) # create LED display object from class
+  print('ok')
   while True:
     print('hi')
     LEDdisplay.randomwalk()
