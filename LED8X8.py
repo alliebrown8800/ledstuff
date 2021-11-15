6f9cab49b8f781144f36b7fd6ff0abbbff384c52
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-15 18:14:07 +0000

    idk

--- LED8X8.py
+++ LED8X8.py
@@ -3,7 +3,7 @@ import time
 import multiprocessing
 import random
 
-class LED8x8():
+class LED8x8(multiprocessing.Process):
 
   # Initial lightning bug:
   pattern = multiprocessing.Array('i',[0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11110111, 0b11111111, 0b11111111, 0b11111111])
