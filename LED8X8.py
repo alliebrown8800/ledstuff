bcc429ab93214bfd6ce7acee199c1ede13cc4aad
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-15 17:56:39 +0000

    firefly

--- LED8X8.py
+++ LED8X8.py
@@ -1,11 +1,14 @@
 from shifter import Shifter # extend shifter by composition
 import time
 import multiprocessing
+import random
 
 class LED8x8():
 
   # Initial lightning bug:
   pattern = multiprocessing.Array('i',[0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11110111, 0b11111111, 0b11111111, 0b11111111])
+  y = 4 # initial y value
+  movement = [-1,0,1]
   
   def __init__(self, data, latch, clock):
     self.shifter = Shifter(data, latch, clock) # initiate with pins
@@ -21,5 +24,37 @@ class LED8x8():
         self.shifter.latch() # latch the shift registers
         time.sleep(.001) # sleep for 1 ms
   
-  #def randomwalk(self):
+  def randomwalk(self):
+    y_change = random.choice(LED8x8.movement) # change in y direction
+    x_change = random.choice(LED8x8.movement) # change in x direction
+
+    # In the y plane:
+    if y_change == -1 and LED8x8.y == 0: # if it's on the left, leave it
+      y_change = 0
+    elif y_change == 1 and LED8x8.y == 7: # if it's on the right, leave it
+      y_change = 0
+
+    # In the x plane:
+    mask = 0b11111111
+    LED8x8.pattern[LED8x8.y] = ~LED8x8.pattern[LED8x8.y] & mask
+
+    if LED8x8.pattern[LED8x8.y] == 0b00000001 and x_change == 1: # if it's on the right, leave it
+      x_change = 0
+    if LED8x8.pattern[LED8x8.y] == 0b10000000 and x_change == -1: # if it's on the left, leave it
+      x_change = 0
+
+    if x_change == -1: LED8x8.pattern[LED8x8.y] = LED8x8.pattern[LED8x8.y] << 1
+    if x_change ==  1: LED8x8.pattern[LED8x8.y] = LED8x8.pattern[LED8x8.y] >> 1
+
+    if y_change != 0:
+      LED8x8.pattern[LED8x8.y + y_change] = ~LED8x8.pattern[LED8x8.y] & mask
+      LED8x8.pattern[LED8x8.y] = 0b11111111
+    else:
+      LED8x8.pattern[LED8x8.y] = ~LED8x8.pattern[LED8x8.y] & mask
+    LED8x8.y = LED8x8.y + y_change
+
+    time.sleep(.5)
+
+
+
 
--- main.py
+++ main.py
@@ -1,21 +1,34 @@
-import multiprocessing
-import time
+import random
 
-class Countdown(multiprocessing.Process):
-  def __init__(self, count, process_name):
-    multiprocessing.Process.__init__(self, name=process_name)
-    self.count = count
-  def run(self):
-    print("Process started")
-    for i in self.count:
-      time.sleep(0.5)
-      print(i)
-    print("Process ended")
+pattern = [0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111111, 0b11111110]
+y = 7 # initial y value
+movement = [-1,0,1]
 
-for i in range(3):
-  p = Countdown([3,2,1], "name="+str(i))
-  p.start()
-  print('p.is_alive() =', p.is_alive())
-  print(p.name)
-  p.join()
-  print('p.is_alive() =', p.is_alive())
+y_change = random.choice(movement) # change in y direction
+x_change = random.choice(movement) # change in x direction
+
+# In the y plane:
+if y_change == -1 and y == 0: # if it's on the left, leave it
+  y_change = 0
+elif y_change == 1 and y == 7: # if it's on the right, leave it
+  y_change = 0
+
+# In the x plane:
+mask = 0b11111111
+pattern[y] = ~pattern[y] & mask
+
+if pattern[y] == 0b00000001 and x_change == 1: # if it's on the right, leave it
+  x_change = 0
+if pattern[y] == 0b10000000 and x_change == -1: # if it's on the left, leave it
+  x_change = 0
+
+if x_change == -1: pattern[y] = pattern[y] << 1
+if x_change ==  1: pattern[y] = pattern[y] >> 1
+
+if y_change != 0:
+  pattern[y + y_change] = ~pattern[y] & mask
+  pattern[y] = 0b11111111
+else:
+  pattern[y] = ~pattern[y] & mask
+y = y + y_change
+
