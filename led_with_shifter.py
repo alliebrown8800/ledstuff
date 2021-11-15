2941c7a3e49ec6b76f64c006d6f646f4328439e9
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-15 15:34:45 +0000

    all on

--- led_with_shifter.py
+++ led_with_shifter.py
@@ -11,7 +11,7 @@ from led_display import LEDdisplay
 dataPin, latchPin, clockPin = 21, 20, 16
 
 # Pick a number sequence
-sequence = [1,2,3]
+sequence = [0]
 
 theLEDdisplay= LEDdisplay(dataPin, latchPin, clockPin)
 
